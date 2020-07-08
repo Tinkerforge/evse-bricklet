f81190744f11b7044438a9d274a8110b031c0de0
Olaf Lüke <olaf@tinkerforge.com>
2020-07-08 14:47:36 +0200

    Add uptime for state to tests

--- tests/log.py
+++ tests/log.py
@@ -9,6 +9,7 @@ from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_evse import BrickletEVSE
 import time
 import csv
+import traceback
 
 LOW_LEVEL_PASSWORD = 0x4223B00B
 
@@ -21,11 +22,15 @@ if __name__ == "__main__":
 
     f = open('log.csv', 'w')
     writer = csv.writer(f)
-    writer.writerow(["Time", "IEC61851 State", "LED State", "Resistance CP/PE", "Resistance PP/PE", "CP PWM Duty Cycle", "Contactor State", "Contactor Check Error", "GPIO Enable", "GPIO LED", "GPIO Motor Switch", "Lock State", "Jumper Configuration", "Lock Switch"])
+    writer.writerow(["Time", "IEC61851 State", "LED State", "Resistance CP/PE", "Resistance PP/PE", "CP PWM Duty Cycle", "Contactor State", "Contactor Check Error", "GPIO Enable", "GPIO LED", "GPIO Motor Switch", "Lock State", "Jumper Configuration", "Lock Switch", "Uptime"])
     while True:
         time.sleep(0.2)
-        state = evse.get_state()
-        line = [str(int(time.time()*10)), state.iec61851_state, state.led_state, state.resistance[0], state.resistance[1], state.cp_pwm_duty_cycle, state.contactor_state, state.contactor_error, state.gpio[0], state.gpio[1], state.gpio[2], state.gpio[3], state.lock_state, state.jumper_configuration]
-        writer.writerow(line)
+        try:
+            state = evse.get_state()
+            line = [str(int(time.time()*10)), state.iec61851_state, state.led_state, state.resistance[0], state.resistance[1], state.cp_pwm_duty_cycle, state.contactor_state, state.contactor_error, state.gpio[0], state.gpio[1], state.gpio[2], state.gpio[3], state.lock_state, state.jumper_configuration, state.uptime]
+            writer.writerow(line)
+        except:
+            print("Time: {0}".format(time.time()))
+            traceback.print_exc()
 
     ipcon.disconnect()
